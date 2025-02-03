722c293a6d1eea29b3a01d5e0e87d6293534f927
marcushoja0108 <marcushj@live.no>
2025-02-03 15:03:11 +0100

    Initial commit

---
+++ app.js
@@ -0,0 +1,90 @@
+// variabler
+const app = Vue.createApp({
+    data(){
+        return {
+            title: 'Game of Thrones',
+            author: 'George R.R. Martin',
+            age: 200
+        }
+    },
+    methods: {
+        changeTitle(title){
+            this.title = title
+        }
+    }
+})
+
+app.mount('#app')
+
+// mouse events
+const app2 = Vue.createApp({
+    data(){
+        return {
+            showBooks: true,
+            title: 'Narnia',
+            author: 'C.S. Lewis',
+            age: 50,
+            x: 0,
+            y: 0,
+        }
+    },
+    methods:{
+        toggleShowBooks(){
+            this.showBooks = !this.showBooks
+        },
+        handleEvent(e, data){
+            console.log(e, e.type)
+            if(data){
+                console.log(data)
+            }
+        },
+        handleMousemove(e){
+            this.x = e.offsetX
+            this.y = e.offsetY
+        }
+    }
+})
+
+app2.mount('#app2')
+
+
+// Lists og output, attribute binding
+const app3 = Vue.createApp({
+    data(){
+        return {
+            url: 'https://www.youtube.com/',
+            showBooks: true,
+            books: [
+                {
+                    title: 'The Lord of the Rings',
+                    author: 'J.R.R Tolkien',
+                    img: "jpg/1.jpg",
+                    isFav: true
+                },{
+                    title: 'Narnia',
+                    author: 'C.S. Lewis',
+                    img: "jpg/2.jpg",
+                    isFav: false
+                },{
+                    title: 'Game of Thrones',
+                    author: 'George R.R. Martin',
+                    img: "jpg/3.jpg",
+                    isFav: true
+                }
+            ]
+        }
+    },
+    methods: {
+        toggleFav(book) {
+            book.isFav = !book.isFav
+        },
+    },
+    //computed er metoder som er avhenging av annen data
+    computed: { 
+        filteredBooks(){
+
+        }
+    }
+})
+
+app3.mount('#app3')
